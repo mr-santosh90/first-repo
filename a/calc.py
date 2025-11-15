bcb0d30473fd8290560e95b695232538a30097e6
mr-santosh90 <santoshbudha730@gmail.com>
2025-11-15 22:07:14 +0545

    made a calculator program in python

---
+++ a/calc.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number : "))
+num2=int(input("enter second number : "))
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+mod=num1%num2
+print("the sum is",sum)
+print("the diff is",sub)
+print("the multile is",mul)
+print("the modulus is",mod)
